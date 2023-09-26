dbea20106b562b627a31a352d6f56f61f843f3f8
Roger Lipscombe <roger@differentpla.net>
2023-09-26 14:42:53 +0100

    Add failing test with a call stack

--- test/failing_SUITE.erl
+++ test/failing_SUITE.erl
@@ -1,11 +1,22 @@
 -module(failing_SUITE).
 -export([
     all/0,
-    fail/1
+    assert_false/1,
+    call_stack/1
 ]).
 
 -include_lib("eunit/include/eunit.hrl").
 
-all() -> [fail].
+all() ->
+    [
+        assert_false,
+        call_stack
+    ].
 
-fail(_Config) -> ?assert(false).
+assert_false(_Config) -> ?assert(false).
+
+call_stack(_Config) -> recursive_call(5).
+
+recursive_call(N) when N > 0 ->
+    % It'll throw a function_clause error; that's deliberate.
+    recursive_call(N - 1).
