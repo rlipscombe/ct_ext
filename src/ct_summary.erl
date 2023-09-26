72c831fb028cdfeadfc93d44d7018776c0396a7d
Roger Lipscombe <roger@differentpla.net>
2023-09-26 10:55:01 +0100

    Report elapsed time in summary

--- src/ct_summary.erl
+++ src/ct_summary.erl
@@ -1,6 +1,7 @@
 -module(ct_summary).
 -export([
     init/2,
+    pre_init_per_testcase/4,
     post_end_per_testcase/5,
     on_tc_skip/4,
     terminate/1
@@ -10,6 +11,9 @@
 -include("glyphs.hrl").
 
 -record(state, {
+    % erlang:monotonic_time, native units.
+    case_started_at,
+    % list of completed test cases, with results
     cases = []
 }).
 
@@ -20,28 +24,72 @@ init(_Id, _Opts) ->
     State = #state{},
     {ok, State}.
 
-post_end_per_testcase(Suite, TestCase, _Config, Return = ok, State = #state{cases = Cases}) ->
-    {Return, State#state{cases = [{passed, Suite, TestCase} | Cases]}};
-post_end_per_testcase(Suite, TestCase, _Config, Return = {error, _}, State = #state{cases = Cases}) ->
-    {Return, State#state{cases = [{failed, Suite, TestCase} | Cases]}}.
+pre_init_per_testcase(_Suite, _TestCase, InitData, State) ->
+    {InitData, State#state{case_started_at = erlang:monotonic_time()}}.
 
-on_tc_skip(Suite, TestCase, _Reason, State = #state{cases = Cases}) ->
-    State#state{cases = [{skipped, Suite, TestCase} | Cases]}.
+post_end_per_testcase(
+    Suite,
+    TestCase,
+    _Config,
+    Return = ok,
+    State = #state{case_started_at = StartedAt, cases = Cases}
+) ->
+    EndedAt = erlang:monotonic_time(),
+    {Return, State#state{cases = [{passed, Suite, TestCase, StartedAt, EndedAt} | Cases]}};
+post_end_per_testcase(
+    Suite,
+    TestCase,
+    _Config,
+    Return = {error, _},
+    State = #state{case_started_at = StartedAt, cases = Cases}
+) ->
+    EndedAt = erlang:monotonic_time(),
+    {Return, State#state{cases = [{failed, Suite, TestCase, StartedAt, EndedAt} | Cases]}}.
+
+on_tc_skip(Suite, TestCase, _Reason, State = #state{case_started_at = StartedAt, cases = Cases}) ->
+    EndedAt = erlang:monotonic_time(),
+    State#state{cases = [{skipped, Suite, TestCase, StartedAt, EndedAt} | Cases]}.
 
 terminate(_State = #state{cases = Cases}) ->
     lists:foreach(fun report/1, Cases),
     io:put_chars(user, ["\e[0m", "\r\n"]).
 
-report({passed, Suite, TestCase}) ->
-    io:put_chars(user, ["  ", color(passed), ?TEST_PASSED_GLYPH, " ", io_lib:format("~s.~s", [Suite, TestCase]), " passed", eol()]);
-report({failed, Suite, TestCase}) ->
-    io:put_chars(user, ["  ", color(failed), ?TEST_FAILED_GLYPH, " ", io_lib:format("~s.~s", [Suite, TestCase]), " failed", eol()]);
-report({skipped, Suite, TestCase}) ->
-    io:put_chars(user, ["  ", color(skipped), ?TEST_SKIPPED_GLYPH, " ", io_lib:format("~s.~s", [Suite, TestCase]), " skipped", eol()]).
+report({passed, Suite, TestCase, StartedAt, EndedAt}) ->
+    report_test_case(
+        color(passed), ?TEST_PASSED_GLYPH, Suite, TestCase, " passed", StartedAt, EndedAt
+    );
+report({failed, Suite, TestCase, StartedAt, EndedAt}) ->
+    report_test_case(
+        color(failed), ?TEST_FAILED_GLYPH, Suite, TestCase, " failed", StartedAt, EndedAt
+    );
+report({skipped, Suite, TestCase, StartedAt, EndedAt}) ->
+    report_test_case(
+        color(skipped), ?TEST_SKIPPED_GLYPH, Suite, TestCase, " skipped", StartedAt, EndedAt
+    ).
+
+report_test_case(Color, Glyph, Suite, TestCase, Suffix, StartedAt, EndedAt) ->
+    io:put_chars(user, [
+        "  ",
+        Color,
+        Glyph,
+        " ",
+        io_lib:format("~s.~s", [Suite, TestCase]),
+        Suffix,
+        format_elapsed_time(EndedAt - StartedAt),
+        eol()
+    ]).
 
 color(passed) -> ?COLOR_GREEN;
 color(failed) -> ?COLOR_RED;
 color(skipped) -> ?COLOR_YELLOW.
+
+format_elapsed_time(Elapsed) ->
+    ElapsedMs = erlang:convert_time_unit(Elapsed, native, millisecond),
+    [" (", format_elapsed_time_ms(ElapsedMs), ")"].
+
+format_elapsed_time_ms(ElapsedMs) ->
+    % TODO: Human readable timestamps for longer periods.
+    io_lib:format("~Bms", [ElapsedMs]).
 
 eol() ->
     ["\e[0m", "\r\n"].
