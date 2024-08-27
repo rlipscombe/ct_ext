f2918bdeb96c44a9a90fb46f0e312b73edb1352e
Roger Lipscombe <roger@differentpla.net>
2024-08-27 12:09:09 +0100

    Report test counts at the end (#4)

--- src/ct_ext_summary.erl
+++ src/ct_ext_summary.erl
@@ -16,6 +16,8 @@
     cases = []
 }).
 
+-include("glyphs.hrl").
+
 -define(APPLICATION, ct_ext).
 
 init(_Id, _Opts) ->
@@ -103,6 +105,37 @@ on_tc_skip(Suite, TestCase, Reason, State) ->
 on_tc_fail(Suite, TestCase, Reason, State) ->
     add_failure(Suite, TestCase, Reason, State).
 
-terminate(_State = #state{cases = Cases}) ->
+terminate(State) ->
+    report_results(State),
+    report_counts(State),
+    ok.
+
+report_results(_State = #state{cases = Cases}) ->
     lists:foreach(fun ct_ext_report:report/1, lists:reverse(Cases)),
     io:put_chars(user, [ct_ext_color:reset(), "\r\n"]).
+
+report_counts(State) ->
+    {PassedCount, SkippedCount, FailedCount} = aggregate_counts(State),
+    io:put_chars(user, [
+        "  ",
+        format_count(PassedCount, passed, ?TEST_PASSED_GLYPH, "passed"),
+        ", ",
+        format_count(SkippedCount, skipped, ?TEST_SKIPPED_GLYPH, "skipped"),
+        ", ",
+        format_count(FailedCount, failed, ?TEST_FAILED_GLYPH, "failed"),
+        " cases",
+        ct_ext_color:eol()
+    ]).
+
+aggregate_counts(_State = #state{cases = Cases}) ->
+    lists:foldl(fun aggregate_counts/2, {0, 0, 0}, Cases).
+
+aggregate_counts({passed, _, _, _, _}, {Passed, Skipped, Failed}) ->
+    {Passed + 1, Skipped, Failed};
+aggregate_counts({skipped, _, _, _, _, _}, {Passed, Skipped, Failed}) ->
+    {Passed, Skipped + 1, Failed};
+aggregate_counts({failed, _, _, _, _, _}, {Passed, Skipped, Failed}) ->
+    {Passed, Skipped, Failed + 1}.
+
+format_count(Count, Key, Glyph, Class) ->
+    [ct_ext_color:color(Key), Glyph, " ", io_lib:format("~B ~s", [Count, Class]), ct_ext_color:reset()].
