5d42b45566f746726bb047a39da2b315efee81b5
Roger Lipscombe <roger@differentpla.net>
2024-08-27 12:52:20 +0100

    Human-readable elapsed time formatting

--- src/ct_ext_elapsed.erl
+++ src/ct_ext_elapsed.erl
@@ -26,9 +26,17 @@ format_elapsed_time(Elapsed, Thresholds) ->
         ct_ext_color:reset()
     ].
 
+format_elapsed_time_ms(ElapsedMs) when ElapsedMs < 1000 ->
+    io_lib:format("~Bms", [ElapsedMs]);
+format_elapsed_time_ms(ElapsedMs) when ElapsedMs < 60_000 ->
+    Seconds = ElapsedMs div 1000,
+    Milliseconds = ElapsedMs rem 1000,
+    io_lib:format("~B.~3..0Bs", [Seconds, Milliseconds]);
 format_elapsed_time_ms(ElapsedMs) ->
-    % TODO: Human readable timestamps for longer periods.
-    io_lib:format("~Bms", [ElapsedMs]).
+    TotalSeconds = ElapsedMs div 1000,
+    Minutes = TotalSeconds div 60,
+    Seconds = TotalSeconds rem 60,
+    io_lib:format("~B.~3..0Bs", [Minutes, Seconds]).
 
 get_elapsed_thresholds() ->
     application:get_env(?APPLICATION, elapsed_thresholds, default_elapsed_thresholds()).
